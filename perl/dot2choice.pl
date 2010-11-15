0aba3b52c008a5f266fd04751920cb858c678af1
Ian Holmes <ihh@berkeley.edu>
2010-11-15 13:00:05 -0800

    Fixed some bugs, preserve dotfile order, allow chains

--- perl/dot2choice.pl
+++ perl/dot2choice.pl
@@ -5,7 +5,7 @@ if (grep (/^-h/, @ARGV)) {
     die
 	"\nUsage: $0 <dot file>\n\n",
 	"Converts graphviz dot-format files into ChoiceScript stubs.\n\n",
-	"Currently a bit picky about the input format (max one edge per line, etc.)\n",
+	"Currently a bit picky about the input format (one edge per line is safest).\n",
 	"Probably best to to filter input through 'dot -Tdot' to clean it up.\n\n",
 	"Use 'label' node/edge attributes for narrative text,\n",
 	"and 'tooltip' edge attribute for choice text.\n\n";
@@ -16,23 +16,56 @@ my @dot = <>;
 grep (chomp, @dot);
 
 # parse file
-my $nameCharRegex = '[A-Za-z0-9_]';
-my $transRegex = '^\s*('.$nameCharRegex.'+?)\s*\->\s*('.$nameCharRegex.'+)\s*(.*?)\s*;?\s*$';
-my $nodeRegex = '^\s*('.$nameCharRegex.'+)\s*(.*?)\s*;?\s*$';
-my @lines = grep (/$transRegex/ || (!/^\s*(node|edge|graph|digraph)\s+/ && !/^\s*\S*?[=\{\}\/\*]/), @dot);
+my $nameRegex = '[A-Za-z_][A-Za-z_\d]*';
+my $nameListRegex = '[A-Za-z0-9_\s]+?';
+my $transRegex = '^\s*('.$nameRegex.'?)\s*\->\s*('.$nameRegex.')\s*(.*?)\s*;?\s*$';
+my $multiRegex = '^\s*('.$nameRegex.'?)\s*\->\s*\{\s*('.$nameListRegex.')\s*\}\s*(.*?)\s*;?\s*$';
+my $nodeRegex = '^\s*('.$nameRegex.')\s*(.*?)\s*;?\s*$';
+my $keywordRegex = '^\s*(node|edge|graph|digraph)\b';
+my $assignRegex = '^\s*'.$nameRegex.'\s*=';
+my $braceRegex = '^\s*[\{\}\[\]]';
 
-my @trans = map (/$transRegex/ ? [$1,$2,$3] : () , @lines);
+# get all relevant lines from file
+my @lines = grep ($dot[$_] !~ /$keywordRegex/
+		  && $dot[$_] !~ /$assignRegex/
+		  && ($dot[$_] =~ /$transRegex/
+		      || $dot[$_] =~ /$multiRegex/
+		      || $dot[$_] =~ /$nodeRegex/),
+		  0..$#dot);
+
+# single transitions
+my @trans;
+for my $src_dest_attr_lnum (map ($dot[$_] =~ /$transRegex/ ? [$1,$2,$3,$_] : () , @lines)) {
+    my ($src, $dest, $attr, $lnum) = @$src_dest_attr_lnum;
+    my @dest;
+    while (1) {
+	push @dest, $dest;
+	last unless "$dest $attr" =~ /$transRegex/;  # unpack chains like a->b->c->d
+	($dest, $attr) = ($2, $3);
+    }
+    for my $d (@dest) { push @trans, [$src,$d,$attr,$lnum]; $src = $d; $lnum += 1/@dest; }
+}
+
+# unpack multiple transitions like a->{b c}
+for my $src_dest_attr_lnum (map ($dot[$_] =~ /$multiRegex/ ? [$1,$2,$3,$_] : () , @lines)) {
+    my ($src, $dests, $attr, $lnum) = @$src_dest_attr_lnum;
+    my @dest = split /\s+/, $dests;
+    for my $dest (@dest) { push @trans, [$src,$dest,$attr,$lnum]; $lnum += 1/@dest }
+}
+
+# nodes
 my %node_attr = (map (/$nodeRegex/ ? ($1=>$2) : (),
 		      grep (!/\->/, @lines)),
-		 map (($_->[0] => ""), @trans));
+		 map (($_->[0] => "", $_->[1] => ""), @trans));
 my @node = keys %node_attr;
 
 # ensure the first node mentioned in the dotfile is the first in @node, and therefore, the first in the scene
+# (beyond this, the ordering of nodes in the choicescript file doesn't matter too much)
 if (@lines) {
     my $first;
-    if ($lines[0] =~ /$transRegex/) {
+    if ($dot[$lines[0]] =~ /$transRegex/) {
 	$first = $1;
-    } elsif ($lines[0] =~ /$nodeRegex/) {
+    } elsif ($dot[$lines[0]] =~ /$nodeRegex/) {
 	$first = $1;
     }
     if (defined $first) {
@@ -52,30 +85,32 @@ while (exists $node_attr{$endScene}) { $endScene .= "_" }
 
 # sort transitions by source
 my %choice;
-grep (push(@{$choice{$_->[0]}}, [@$_[1,2]]), @trans);
+grep (push(@{$choice{$_->[0]}}, [@$_[1,2,3]]), @trans);
 
 # loop over sources
 for my $node (@node) {
     print map ("$_\n",
 	       "*comment $node;",
 	       "*label $node",
-	       getAttr ($node_attr{$node}, "label", "This is $node."));
+	       getAttr ($node_attr{$node}, "label", "Currently: $node."));
     if (defined $choice{$node} && @{$choice{$node}} > 1) {
 	print "*choice\n";
-	for my $dest_attrs (@{$choice{$node}}) {
-	    my ($dest, $attrs) = @$dest_attrs;
+	my @choices = sort { $a->[2] <=> $b->[2] } @{$choice{$node}};
+	for my $dest_attrs_lnum (@choices) {
+	    my ($dest, $attrs, $lnum) = @$dest_attrs_lnum;
 	    my $tip = getAttr ($attrs, "tooltip", "$dest");
 	    my $label = getAttr ($attrs, "label", "You choose $dest.");
 	    print map ("  $_\n",
-		       "*comment $node -> $dest;",
+		       "*comment $node -> $dest;   // line " . int($lnum+1),
 		       "#$tip",
-		       $label,
-		       "*goto $dest",
+		       "  $label",
+		       "  *page_break",
+		       "  *goto $dest",
 		       "");
 	}
 	print "\n";
     } elsif (defined $choice{$node} && @{$choice{$node}} == 1) {
-	my ($dest, $attrs) = @{$choice{$node}->[0]};
+	my ($dest, $attrs, $lnum) = @{$choice{$node}->[0]};
 	my $label = getAttr ($attrs, "label", "You choose $dest.");
 	print map ("$_\n",
 		   "*line_break",
